52b669f753c06217a2ac755e5585fcc5de9b8213
Alexandre Versace <yagamiztc123@gmail.com>
2020-07-05 13:38:01 -0300

    Update ParticleCollision.cs

--- ParticleCollision.cs
+++ ParticleCollision.cs
@@ -12,7 +12,7 @@ public class ParticleCollision : MonoBehaviour
         ParticlePhysicsExtensions.GetCollisionEvents(particleSplat, other, collisionEvents);
 
         int count = collisionEvents.Count;
-
+        //very good way of optmizing for loops, instead of counting the amount of collision every loop, jut store it in a variable once
         for (int i = 0; i < count; i++){
             GameObject splat = Instantiate(splatPrefab, collisionEvents[i].intersection, Quaternion.identity);
             ParticleSplat ps = splat.GetComponent<ParticleSplat>();
